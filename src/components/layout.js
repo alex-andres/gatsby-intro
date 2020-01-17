ee323ce2ece313de56c4106a03fd09faf0b7a0f9
c-andres <charlesalexanderandres@gmail.com>
2020-01-17 06:01:28 -0800

    mac commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
+
+const Layout = ({ children }) => {
+  <>
+    <Global 
+    styles={css`
+      *{ box-sizing: border-box;
+      margin: 0;
+    `} />
+  </>
+}
